af7f400ff32c2df16b36a7f2d2d6133af76c0d45
Memitaru <amandatwah@gmail.com>
2019-12-12 19:25:10 -0800

    finished first pass for stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,17 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  best_profit = None
+  for i in range(0, len(prices)):
+    for j in range(0, len(prices)):
+      if j > i:
+        if best_profit == None:
+          best_profit = prices[j]-prices[i]
+        if (prices[j]-prices[i]) > best_profit:
+          best_profit = prices[j]-prices[i]
+  return best_profit
+
+print(find_max_profit([100, 90, 80, 50, 20, 10]))
 
 
 if __name__ == '__main__':
